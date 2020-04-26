a1dc9c277e6ed317c37060d96303af6652340eb1
shravankumar9892 <shravankumarshetty9892@gmail.com>
2020-04-26 21:08:03 +0530

    App built without design

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -12,16 +12,26 @@ export class BoardComponent implements OnInit {
 
   constructor() { }
 
+  /**
+   * Lifehook cycle of the Board component
+   */
   ngOnInit() {
     this.newGame()
   }
 
+  /**
+   * Resets & initializes objects for a new game
+   */
   newGame(){
     this.squares = Array(9).fill(null)
     this.winner = null
     this.xisNext = true
   }
 
+  /**
+   * player() -> current player value
+   * Either X or O
+   */
   get player() {
     if(this.xisNext){
       return 'X'
@@ -29,5 +39,46 @@ export class BoardComponent implements OnInit {
     else {
       return 'O'
     }
+  }
+
+  /**
+   * 
+   * @param idx Index of the square clicked by player
+   * makeMove() -> sets the value of square and objects
+   */
+  makeMove(idx: number) {
+    if (!this.squares[idx]) {
+      // If the square is blank
+      this.squares.splice(idx, 1, this.player)
+      this.xisNext = !this.xisNext
+
+    }
+
+    this.winner = this.calculateWinner(this.squares)
+  }
+
+  /**
+   * 
+   * @param squares Represents current state of 9 squares of a tic tac toe
+   * calculateWinner() -> returns X or O or null (winner)
+   */
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 }
